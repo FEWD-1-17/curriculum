c7dd72506492cb481d7e81ff5ea311bf5791aa48
Tor N. Johnson <kasigi@gmail.com>
2023-03-21 17:36:40 -0400

    colors

---
+++ Exercises and Homework/Optional Lessons/NodeJS/01_colors/solution_code/index.js
@@ -0,0 +1,13 @@
+const colors = require("colors");
+
+console.log(colors.green("Hello"))
+
+console.log(colors.red.underline('I like cake and pies'))
+
+console.log(colors.inverse('Inverse the color'))
+
+console.log(colors.rainbow('OMG Rainbows!!'))
+
+console.log(colors.red.inverse('Warning: May include leprechauns'))
+
+console.log(colors.trap('Run the trap'))
